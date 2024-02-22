fe190cade8dd75c5c2d51aec868f72d8346ef42c
liulongling <1805348347@qq.com>
2024-02-22 21:55:24 +0800

    新增多个example示例

---
+++ example/example-db/src/main/java/com/game/example/db/user/common/DataEntity.java
@@ -0,0 +1,17 @@
+package com.game.example.db.user.common;
+
+import lombok.Data;
+
+/**
+ *
+ * @author: liulongling
+ * @date: 2024/2/20
+ */
+@Data
+public class DataEntity {
+
+    /**
+     * 玩家唯一ID
+     */
+    private int uid;
+}
---
+++ example/example-server/src/main/java/com/game/example/logic/action/LoginAction.java
@@ -0,0 +1,67 @@
+package com.game.example.logic.action;
+
+import com.game.example.jproto.LoginVerify;
+import com.game.example.jproto.UserInfo;
+import com.game.server.common.GameCmd;
+import com.iohao.game.action.skeleton.annotation.ActionController;
+import com.iohao.game.action.skeleton.annotation.ActionMethod;
+import com.iohao.game.action.skeleton.core.flow.FlowContext;
+import com.iohao.game.bolt.broker.client.kit.UserIdSettingKit;
+import lombok.extern.slf4j.Slf4j;
+import org.jctools.maps.NonBlockingHashMap;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.LongAdder;
+
+
+@Slf4j
+@ActionController(GameCmd.cmd)
+public class LoginAction {
+
+    final Map<String, Long> userMap = new NonBlockingHashMap<>();
+
+    LongAdder userIdAdder = new LongAdder();
+
+    /**
+     * 登录验证
+     *
+     * @param loginVerify 登录验证pb
+     * @param flowContext f
+     * @return 用户信息
+     */
+    @ActionMethod(GameCmd.loginVerify)
+    public UserInfo loginVerify(LoginVerify loginVerify, FlowContext flowContext) {
+        log.info("loginVerify {} ", loginVerify);
+
+        String jwt = loginVerify.jwt;
+
+        // TODO: 2022/2/6 从数据库中
+        Long newUserId = userMap.get(jwt);
+
+        if (Objects.isNull(newUserId)) {
+            userIdAdder.increment();
+
+            newUserId = userIdAdder.longValue();
+            userMap.put(jwt, newUserId);
+        }
+
+        UserInfo userInfo = new UserInfo();
+        userInfo.id = newUserId;
+        userInfo.name = jwt;
+
+        // 登录的关键代码
+        // 具体可参考 https://www.yuque.com/iohao/game/tywkqv
+        boolean success = UserIdSettingKit.settingUserId(flowContext, newUserId);
+
+        if (!success) {
+            // TODO: 2022/1/19 抛异常码
+        }
+
+        userMap.clear();
+
+        return userInfo;
+    }
+
+
+}
